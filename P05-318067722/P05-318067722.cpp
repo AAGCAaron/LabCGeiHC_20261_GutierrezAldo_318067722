b80bd23c869e63e1afaa7571bac29d2943a76028
AAGCAaron <AGC.Aaron.13@gmail.com>
2025-09-28 06:55:55 -0600

    Practica 05
    Modelamos en blender un carrito y lo animamos para que se moviera

---
+++ P05-318067722/P05-318067722.cpp
@@ -0,0 +1,269 @@
+/*
+Práctica 5: Optimización y Carga de Modelos
+*/
+//para cargar imagen
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+#include <math.h>
+
+#include <glew.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+//para probar el importer
+//#include<assimp/Importer.hpp>
+
+#include "Window.h"
+#include "Mesh.h"
+#include "Shader_m.h"
+#include "Camera.h"
+#include "Sphere.h"
+#include"Model.h"
+#include "Skybox.h"
+
+const float toRadians = 3.14159265f / 180.0f;
+//float angulocola = 0.0f;
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+Camera camera;
+
+Model CarroBase;
+Model CarroCofre;
+Model Llanta1;
+Model Llanta2;
+Model Llanta3;
+Model Llanta4;
+
+
+Skybox skybox;
+
+//Sphere cabeza = Sphere(0.5, 20, 20);
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+
+
+// Vertex Shader
+static const char* vShader = "shaders/shader_m.vert";
+
+// Fragment Shader
+static const char* fShader = "shaders/shader_m.frag";
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+		//	x      y      z			u	  v			nx	  ny    nz
+			-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,
+			1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f
+	};
+
+	unsigned int floorIndices[] = {
+		0, 2, 1,
+		1, 2, 3
+	};
+
+	GLfloat floorVertices[] = {
+		-10.0f, 0.0f, -10.0f,	0.0f, 0.0f,		0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, -10.0f,	10.0f, 0.0f,	0.0f, -1.0f, 0.0f,
+		-10.0f, 0.0f, 10.0f,	0.0f, 10.0f,	0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, 10.0f,		10.0f, 10.0f,	0.0f, -1.0f, 0.0f
+	};
+
+	
+	Mesh *obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj1);
+
+	Mesh *obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj2);
+
+	Mesh *obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
+
+
+}
+
+
+void CreateShaders()
+{
+	Shader *shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
+
+
+
+int main()
+{
+	mainWindow = Window(1366, 768); // 1280, 1024 or 1024, 768
+	mainWindow.Initialise();
+
+	CreateObjects();
+	CreateShaders();
+
+	camera = Camera(glm::vec3(0.0f, 0.5f, 7.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 1.0f);
+
+
+	CarroBase = Model();
+	CarroBase.LoadModel("Models/CarroBase.fbx");
+	CarroCofre = Model();
+	CarroCofre.LoadModel("Models/CarroCofre.fbx");
+	Llanta1 = Model();
+	Llanta1.LoadModel("Models/Llanta1.fbx");
+	Llanta2 = Model();
+	Llanta2.LoadModel("Models/Llanta2.fbx");
+	Llanta3 = Model();
+	Llanta3.LoadModel("Models/Llanta3.fbx");
+	Llanta4 = Model();
+	Llanta4.LoadModel("Models/Llanta4.fbx");
+
+
+	std::vector<std::string> skyboxFaces;
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_rt.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_lf.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_dn.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_up.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_bk.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_ft.tga");
+
+	skybox = Skybox(skyboxFaces);
+
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
+		uniformSpecularIntensity = 0, uniformShininess = 0;
+	GLuint uniformColor = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
+	
+
+	glm::mat4 model(1.0);
+	glm::mat4 modelaux(1.0);
+	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
+
+	////Loop mientras no se cierra la ventana
+	while (!mainWindow.getShouldClose())
+	{
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		deltaTime += (now - lastTime) / limitFPS;
+		lastTime = now;
+
+		//Recibir eventos del usuario
+		glfwPollEvents();
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+		// Clear the window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//Se dibuja el Skybox
+		skybox.DrawSkybox(camera.calculateViewMatrix(), projection);
+
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+		uniformView = shaderList[0].GetViewLocation();
+		uniformColor = shaderList[0].getColorLocation();
+
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+		// INICIA DIBUJO DEL PISO
+		color = glm::vec3(0.5f, 0.5f, 0.5f); //piso de color gris
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		meshList[2]->RenderMesh();
+
+		// ====== Base del carro ======
+		color = glm::vec3(0.0f, 0.0f, 1.0f); // azul
+
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f + 0.1f * mainWindow.getarticulacion1()));
+
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+
+		// Guardamos para usarlo como base del cofre
+		modelaux = model;
+
+		// Rotamos la base para orientarla bien
+		model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		CarroBase.RenderModel();
+
+
+		// ====== Cofre del carro (jerarquía tipo mandíbula) ======
+		modelaux = glm::rotate(modelaux, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)); // rotación del carro base
+
+		// Ahora tratamos al cofre como parte jerárquica
+		modelaux = glm::translate(modelaux, glm::vec3(0.0f, -1.3f, 0.20f)); // mover al lugar donde va el cofre
+		modelaux = glm::rotate(modelaux, glm::radians(mainWindow.getarticulacion3()), glm::vec3(1.0f, 0.0f, 0.0f)); // rotar el cofre
+
+		// Render
+		color = glm::vec3(1.0f, 0.0f, 0.0f); // rojo
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelaux));
+		CarroCofre.RenderModel();
+
+		// ====== Llanta Izquierda delantera ======
+		modelaux = model;
+		modelaux = glm::translate(modelaux, glm::vec3(-0.65f, -0.8f, -0.3f));
+		modelaux = glm::rotate(modelaux, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelaux));
+		Llanta1.RenderModel();
+
+		// ====== Llanta Derecha delantera ======
+		modelaux = model;
+		modelaux = glm::translate(modelaux, glm::vec3(0.65f, -0.8f, -0.3f));
+		modelaux = glm::rotate(modelaux, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelaux));
+		Llanta2.RenderModel();
+
+		// ====== Llanta Izquierda trasera =====
+		
+		modelaux = model;
+		modelaux = glm::translate(modelaux, glm::vec3(-0.65f, -3.1f, -0.3f));
+		modelaux = glm::rotate(modelaux, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelaux));
+		Llanta3.RenderModel();
+		
+		// ====== Llanta derecha trasera ======
+		modelaux = model;
+		modelaux = glm::translate(modelaux, glm::vec3(0.65f, -3.1f, -0.3f));
+		modelaux = glm::rotate(modelaux, glm::radians(mainWindow.getarticulacion1()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(modelaux));
+		Llanta4.RenderModel();
+
+
+		
+
+
+
+
+
+		glUseProgram(0);
+
+		mainWindow.swapBuffers();
+	}
+
+	return 0;
+}
